594632e6cc005ebc36ecf70826743406a8534a6a
nanimono <nan1mono@163.com>
2023-12-28 10:30:28 +0800

    feat：秒级定时读取完成

--- src/main/java/com/tecpie/modbus/core/Application.java
+++ src/main/java/com/tecpie/modbus/core/Application.java
@@ -1,14 +1,12 @@
 package com.tecpie.modbus.core;
 
-import com.serotonin.modbus4j.ModbusMaster;
-import com.tecpie.modbus.core.master.ModbusMasterConfig;
 import com.tecpie.modbus.core.schedule.ScheduleRunner;
 import com.tecpie.modbus.entity.CacheTasksConfig;
 import com.tecpie.modbus.toolkit.ConfigReader;
 
 public class Application {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // 读取文件
         CacheTasksConfig cacheTasksConfig = ConfigReader.readCache();
         ScheduleRunner scheduleRunner = new ScheduleRunner();
